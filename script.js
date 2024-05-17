de47f9a6c6568a360ab856723b0ac5ab00e2b658
Flávia Reis <flaviarster@gmail.com>
2024-05-17 11:49:14 -0300

    Finishing the styles

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-let messageArea = document.querySelector('#message')
-
-function adjustTextareaRows() {
-    if (window.innerWidth <= 768) {
-        messageArea.rows = 12
-    } else if (window.innerWidth > 768 && window.innerWidth <= 1280) {
-        messageArea.rows = 8
-    } else {
-        messageArea.rows = 4
-    }
-}
-
-window.onload = adjustTextareaRows
-window.onresize = adjustTextareaRows
+const messageArea = document.querySelector('#message')
