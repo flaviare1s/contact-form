55edbb170c8acacc7aea1a4e7a8b181bc47a74cb
Flávia Reis <flaviarster@gmail.com>
2024-05-17 14:10:21 -0300

    Add form validation functionality

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const messageInput = document.querySelector('#message')
 const messageError = document.querySelector('#messageError')
 const checkboxInput = document.querySelector('#checkbox')
 const checkboxError = document.querySelector('#checkboxError')
-const messageArea = document.querySelector('#message')
 const btnSubmit = document.querySelector('#btnSubmit')
 const radiosContainers = document.querySelectorAll('.radio-container')
 
@@ -30,6 +29,70 @@ document.addEventListener('click', () => {
 
 function validateForm(event) {
     event.preventDefault()
+    let isValid = true
+
+    if(!form.checkValidity()) {
+        if(!firstNameInput.checkValidity()) {
+            firstNameInput.classList.add('errorActive')
+            firstNameError.innerText = 'This field is required'
+            isValid = false
+        } else {
+            firstNameInput.classList.remove('errorActive')
+            firstNameError.innerText = ''
+        }
+
+        if(!lastNameInput.checkValidity()) {
+            lastNameInput.classList.add('errorActive')
+            lastNameError.innerText = 'This field is required'
+            isValid = false
+        } else {
+            lastNameInput.classList.remove('errorActive')
+            lastNameError.innerText = ''
+        }
+
+        if(!emailInput.checkValidity()) {
+            emailInput.classList.add('errorActive')
+            emailInput.value = 'email@example.com'
+            emailError.innerText = 'Please enter a valid email address'
+            isValid = false
+        } else {
+            emailInput.classList.remove('errorActive')
+            emailError.innerText = ''
+        }
+
+        if(!generalRadioInput.checked && !supportRadioInput.checked) {
+            queryError.innerText = 'Please select a query type'
+            radiosContainers.forEach(container => {
+                container.classList.add('errorActive')
+            })
+            isValid = false
+        } else {
+            queryError.innerText = ''
+        }
+
+        if(!messageInput.checkValidity()) {
+            messageInput.classList.add('errorActive')
+            messageError.innerText = 'This field is required'
+            isValid = false
+        } else {
+            messageInput.classList.remove('errorActive')
+            messageError.innerText = ''
+        }
+
+        if(!checkboxInput.checked) {
+            checkboxError.innerText = 'To submit this form, please consent to being contacted'
+            isValid = false
+        } else {
+            checkboxError.innerText = ''
+        }
+
+        if(!isValid) {
+            return
+        }
+
+        form.submit()
+    }
 }
 
 btnSubmit.addEventListener('click', validateForm)
+form.addEventListener('submit', validateForm)
