29c754ed2793f0aef899d1e550bf20f1442cd666
Flávia Reis <flaviarster@gmail.com>
2024-05-17 10:18:10 -0300

    Adjusting the textArea size using JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+function adjustTextareaRows() {
+    let messageArea = document.querySelector('#message')
+    if (window.innerWidth <= 768) {
+        messageArea.rows = 12
+    } else if (window.innerWidth > 768 && window.innerWidth <= 1280) {
+        messageArea.rows = 8
+    } else {
+        messageArea.rows = 4
+    }
+}
+
+window.onload = adjustTextareaRows
+window.onresize = adjustTextareaRows
